f404f59cf5912a7c148c2229321cda9ef3aba824
preetam mondal <preetammondal78@gmail.com>
2023-07-10 00:42:00 +0530

    implementation done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import Signup from './features/auth/components/Signup';
+import SignupPage from './pages/SignupPage';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>,
+  },
+  {
+    path:"/login",
+    element:<LoginPage/>,
+  },
+  {
+    path:"/signup",
+    element:<SignupPage/>
+  }
+])
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <RouterProvider router={router}/>
     </div>
   );
 }
